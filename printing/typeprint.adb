1ef46c27cf9b264a5acf28e7f6606dc442c5fbd8
psyomn <lethaljellybean@gmail.com>
2012-04-07 16:21:26 -0400

    Printing different variable types. Simple.

---
+++ printing/typeprint.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO; 
+use  Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO; 
+
+procedure Typeprint is
+  A : Integer := 123; 
+  B : Float := 123.123; 
+begin
+  Put_Line("Hello world"); 
+  Put(A); 
+  Put(B); 
+end Typeprint; 
+
