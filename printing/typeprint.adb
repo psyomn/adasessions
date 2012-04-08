6cc1291ff7883e2ea6c6f3e525d231817e74c79c
psyomn <lethaljellybean@gmail.com>
2012-04-08 16:06:50 -0400

    Some other very simple tests. I remember enough in this language now in order to write without looking at sample code I just can't wait to see how you include the equivalent of header files in this langauge...

--- printing/typeprint.adb
+++ printing/typeprint.adb
@@ -4,9 +4,18 @@ use  Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO;
 procedure Typeprint is
   A : Integer := 123; 
   B : Float := 123.123; 
+  Booly : Boolean := true;
 begin
   Put_Line("Hello world"); 
   Put(A); 
+  Put_Line(""); 
   Put(B); 
+  Put_Line(""); 
+
+  if Booly then 
+    Put_Line("Booly is true"); 
+  end if; 
+
+  -- Put(Booly);
 end Typeprint; 
 
